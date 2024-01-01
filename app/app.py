7ff6071fabd6618308ef766296b8f8f83e8f198c
Bhanuj Sharma <bhanujsharma8990@gmail.com>
2024-01-01 10:48:21 +0530

    Integrated Model with flask and completed the basic template

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
 cars = pickle.load(open('../exports/cardDict.pkl','rb'))
 cars = pd.DataFrame(cars)
+model = pickle.load(open('../exports/model.pkl','rb'))
 
 
 @app.route('/')
@@ -14,12 +16,20 @@ def home():
     model = sorted(cars['Model'].unique())
     year = sorted(cars['Year'].unique())
     mileage = sorted(cars['Mileage'].unique())
-    def zip_lists(L1, L2):
-        return list(zip(L1, L2))
-    ManModel = zip_lists(manu,model)
-    print(ManModel)
 
-    return render_template('index.html',manu=manu,model=model,year=year,mileage=mileage,ManModel=ManModel)
+    return render_template('index.html',manu=manu,model=model,year=year,mileage=mileage)
+
+@app.route('/process',methods=['POST'])
+def process():
+    manu = request.form.get('manu')
+    CarModel = request.form.get('CarModel')
+    year = int(request.form.get('year'))
+    mileage = int(request.form.get('mileage'))
+
+    predictionDF = pd.DataFrame([[manu,CarModel,year,mileage]],columns=["Make","Model","Year","Mileage"]);
+    pred = model.predict(predictionDF)
+    return str(np.round(pred[0][0],2))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,21 @@
+function handleEvent(e){
+    e.preventDefault();
+}
+function sendData(){
+    document.querySelector('form').addEventListener('submit',handleEvent)
 
+    const fd = new FormData(document.querySelector('form'))
+    const xhr = new XMLHttpRequest()
+
+    xhr.open('POST','/process',true)
+    document.getElementById("prediction").innerHTML = "Processing request ..."
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            document.getElementById("prediction").innerHTML="The reslate price is : $"+xhr.responseText;
+        }
+    }
+    xhr.onload = function(){}
+    xhr.send(fd);
+
+}
